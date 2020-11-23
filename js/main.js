ab6fbe12da008de9209e3064f23f2a7ca681987a
Giulia Napoli <gnapoli1996@gmail.com>
2020-11-23 18:55:51 +0100

    Continues Layout + Adds Vue constance

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const boolzApp = new Vue ({
+  el: '#root',
+  data: {
+    contacts: [
+      {
+        nome: 'Michele',
+        foto: 'resources/img/avatar_1.jpg'
+      },
+      {
+        nome: 'Fabio',
+        foto: 'resources/img/avatar_2.jpg'
+      },
+      {
+        nome: 'Samuele',
+        foto: 'resources/img/avatar_3.jpg'
+      },
+      {
+        nome: 'Luisa',
+        foto: 'resources/img/avatar_4.jpg'
+      }
+    ]
+  }
+})
