042a5e12b4b9fb9e5a6363b8151f40ab7dd6947f
Giulia Napoli <gnapoli1996@gmail.com>
2020-11-24 00:59:44 +0100

    Adds contact username & last-seen

--- js/main.js
+++ js/main.js
@@ -1,23 +1,35 @@
-const boolzApp = new Vue ({
+// dichiaro l'array di contatti
+const contacts = [
+
+  {
+    username: 'Michele',
+    photo: 'resources/img/avatar_1.jpg',
+    lastSeen: '10:30 Lun 20 Ago'
+  },
+  {
+    username: 'Fabio',
+    photo: 'resources/img/avatar_2.jpg',
+    lastSeen: '10:30 Lun 20 Ago'
+  },
+  {
+    username: 'Samuele',
+    photo: 'resources/img/avatar_3.jpg',
+    lastSeen: '10:30 Lun 20 Ago'
+  },
+  {
+    username: 'Luisa',
+    photo: 'resources/img/avatar_4.jpg',
+    lastSeen: '10:30 Lun 20 Ago'
+  }
+]
+
+
+const rootApp = new Vue ({
   el: '#root',
+
   data: {
-    contacts: [
-      {
-        nome: 'Michele',
-        foto: 'resources/img/avatar_1.jpg'
-      },
-      {
-        nome: 'Fabio',
-        foto: 'resources/img/avatar_2.jpg'
-      },
-      {
-        nome: 'Samuele',
-        foto: 'resources/img/avatar_3.jpg'
-      },
-      {
-        nome: 'Luisa',
-        foto: 'resources/img/avatar_4.jpg'
-      }
-    ]
+
+    contacts: [...contacts]
+
   }
 })
