0335874767aefa0718ee17071e9d378ea742db89
Giulia Napoli <gnapoli1996@gmail.com>
2020-11-24 19:45:55 +0100

    Defines layout, input event, creates msg
    Hooks users to personal chat.
    Declares input entering event and generates sended & received msg.
    Gets better layout
    Ive still msg layout bugs

--- js/main.js
+++ js/main.js
@@ -1,25 +1,64 @@
+// dati statici
+const receive = 'received';
+const send = 'sended';
+
+const myUsername = {
+  username: 'Giulia',
+  photo: 'resources/img/avatar_io.jpg',
+  status: ''
+}
+
+
 // dichiaro l'array di contatti
 const contacts = [
 
   {
     username: 'Michele',
     photo: 'resources/img/avatar_1.jpg',
-    lastSeen: '10:30 Lun 20 Ago'
+    lastSeen: '10:30 Lun 20 Ago',
+    chatHistory: [
+      {
+        text: 'Hello, how r u?',
+        date: '22 Nov 2020',
+        type: 'receive'
+      }
+    ]
   },
   {
     username: 'Fabio',
     photo: 'resources/img/avatar_2.jpg',
-    lastSeen: '10:30 Lun 20 Ago'
+    lastSeen: '10:30 Lun 20 Ago',
+    chatHistory: [
+      {
+        text: 'Hello, how r u?',
+        date: '22 Nov 2020',
+        type: 'receive'
+      }
+    ]
   },
   {
     username: 'Samuele',
     photo: 'resources/img/avatar_3.jpg',
-    lastSeen: '10:30 Lun 20 Ago'
+    lastSeen: '10:30 Lun 20 Ago',
+    chatHistory: [
+      {
+        text: 'Hello, how r u?',
+        date: '22 Nov 2020',
+        type: 'receive'
+      }
+    ]
   },
   {
     username: 'Luisa',
     photo: 'resources/img/avatar_4.jpg',
-    lastSeen: '10:30 Lun 20 Ago'
+    lastSeen: '10:30 Lun 20 Ago',
+    chatHistory: [
+      {
+        text: 'Hello, how r u?',
+        date: '22 Nov 2020',
+        type: 'receive'
+      }
+    ]
   }
 ]
 
@@ -29,7 +68,27 @@ const rootApp = new Vue ({
 
   data: {
 
-    contacts: [...contacts]
+    inputMessage: '',
+    contacts: [...contacts],
+    counter: 0,
+    myUsername: myUsername
 
+  },
+  methods: {
+    contactChat: function(index) {
+      this.counter = index;
+    },
+    sendMessage: function() {
+      this.contacts[this.counter].chatHistory.push(
+        {
+          text: this.inputMessage,
+          date: '22 Nov 2020',
+          type: 'send'
+        }
+      )
+
+      this.inputMessage = '';
+    }
   }
+
 })
